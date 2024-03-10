d63befbbab4cf8e80917a8708905e6cb54ba9a46
polyakovgregory WX1104803 <polyakovgregory@huawei-partners.com>
2024-03-10 21:43:53 +0800

    addmodel load

---
+++ evaluation/LLM.py
@@ -0,0 +1,203 @@
+import transformers
+from transformers import GenerationConfig, LlamaForCausalLM, LlamaTokenizer, GenerationMixin
+from transformers.generation.utils import GenerationMode
+from peft import PeftModel
+import torch
+import sys
+
+from utils.callbacks import Iteratorize, Stream, StopSequenceCriteria
+from helpers.ssl import no_ssl_verification
+
+
+if torch.cuda.is_available():
+    device = "cuda"
+else:
+    device = "cpu"
+
+try:
+    if torch.backends.mps.is_available():
+        device = "mps"
+except:  # noqa: E722
+    pass
+
+class LLM(GenerationMixin):
+    @staticmethod
+    def load_llm_tokenizer(
+        base_model, lora_weights=None, load_8bit=None, 
+        load_in_4bit=None, load_from_local=None
+    ) -> tuple[transformers.LlamaTokenizer, transformers.LlamaForCausalLM]:
+        with no_ssl_verification():
+            tokenizer = LlamaTokenizer.from_pretrained(
+                base_model,
+                local_files_only=load_from_local,
+            )
+        if device == "cuda":
+            with no_ssl_verification():
+                print(f"{load_8bit=}")
+                print(f"{load_in_4bit=}")
+                model = LlamaForCausalLM.from_pretrained(
+                    base_model,
+                    load_in_8bit=load_8bit,
+                    load_in_4bit=load_in_4bit,
+                    torch_dtype=torch.float16,
+                    device_map="auto",
+                    local_files_only=load_from_local,
+                )
+            if lora_weights is not None:
+                model = PeftModel.from_pretrained(
+                    model,
+                    lora_weights,
+                    torch_dtype=torch.float16,
+                )
+                print(f"Loaded LORA")
+        elif device == "mps":
+            model = LlamaForCausalLM.from_pretrained(
+                base_model,
+                device_map={"": device},
+                torch_dtype=torch.float16,
+            )
+            if lora_weights is not None:
+                model = PeftModel.from_pretrained(
+                    model,
+                    lora_weights,
+                    device_map={"": device},
+                    torch_dtype=torch.float16,
+                )
+        else:
+            model = LlamaForCausalLM.from_pretrained(
+                base_model, device_map={"": device}, low_cpu_mem_usage=True
+            )
+            if lora_weights is not None:
+                model = PeftModel.from_pretrained(
+                    model,
+                    lora_weights,
+                    device_map={"": device},
+                )
+        # unwind broken decapoda-research config
+        model.config.pad_token_id = tokenizer.pad_token_id = 0  # unk
+        model.config.bos_token_id = 1
+        model.config.eos_token_id = 2
+
+        if not load_8bit and not load_in_4bit:
+            model.half()  # seems to fix bugs for some users.
+
+        model.eval()
+        # if torch.__version__ >= "2" and sys.platform != "win32":
+        #     model = torch.compile(model)
+        return tokenizer, model
+
+    def __init__(
+        self,
+        base_model,
+        lora_weights=None,
+        load_8bit=None,
+        load_in_4bit=None,
+        load_from_local=None,
+    ) -> None:
+        tokenizer, model = self.load_llm_tokenizer(
+            base_model=base_model, lora_weights=lora_weights, 
+            load_8bit=load_8bit, load_in_4bit=load_in_4bit, 
+            load_from_local=load_from_local
+        )
+        self.tokenizer = tokenizer
+        self.model = model
+
+    def generate(self, prompt: str, generate_params: dict = None, 
+                 stop_sequences=None):
+        input_ids = self.tokenizer(prompt, return_tensors="pt")[
+            "input_ids"
+        ].to(device)
+
+        if stop_sequences is None:
+            stop_sequences = [self.tokenizer.eos_token]
+        if generate_params is None:
+            generate_params = {
+                "input_ids": input_ids,
+                "num_beams": 5,
+                "return_dict_in_generate": True,
+                "output_scores": True,
+                "max_new_tokens": 512
+            }
+        else:
+            generate_params.update({"input_ids": input_ids})
+
+        def generate_with_callback(callback=None, **kwargs):
+            kwargs.setdefault(
+                "stopping_criteria", transformers.StoppingCriteriaList()
+            )
+            kwargs["stopping_criteria"].append(Stream(callback_func=callback))
+            kwargs["stopping_criteria"].append(
+                StopSequenceCriteria(stop_sequences, self.tokenizer)
+            )
+            with torch.no_grad():
+                self.model.generate(**kwargs)
+
+        def generate_with_streaming(**kwargs):
+            return Iteratorize(generate_with_callback, kwargs, callback=None)
+
+        with generate_with_streaming(**generate_params) as generator:
+            for output in generator:
+                if output[-1] in [self.tokenizer.eos_token_id]:
+                    break
+
+                decoded_output = self.tokenizer.decode(
+                    output, skip_special_tokens=True
+                )
+                prompt = self.tokenizer.decode(
+                    self.tokenizer.encode(prompt), skip_special_tokens=True
+                )
+                response = decoded_output.removeprefix(prompt)
+                yield response
+
+                is_end_of_gen = False
+                for s in stop_sequences:
+                    if s in response:
+                        is_end_of_gen = True
+                        response = response[: response.index(s) + len(s)]
+                if is_end_of_gen:
+                    yield response
+                    break
+
+    @torch.inference_mode()
+    def generate_eval_(self, prompt: str, generation_config: GenerationConfig):
+        input_ids = self.tokenizer(prompt, return_tensors="pt")[
+            "input_ids"
+        ].to(device)
+        output_ids = input_ids.tolist()[0]
+        stop_token_ids = [self.tokenizer.eos_token_id]
+        # logits_processor = self._get_logits_processor(
+        #     generation_config=generation_config,
+        #     input_ids_seq_length=input_ids.shape[-1],
+        #     encoder_input_ids=input_ids,
+        #     prefix_allowed_tokens_fn=None
+        # )
+        past_key_values = out = token = None
+        generation_mode = self._get_generation_mode(generation_config, None)
+        for i in range(generation_config.max_new_tokens):
+            if i == 0:
+                out = self.model(input_ids, use_cache=True)
+            else:
+                out = self.model(torch.as_tensor([[token]], device=device), use_cache=True, past_key_values=past_key_values)
+            logits = out.logits
+            past_key_values = out.past_key_values
+            last_token_logits = logits[0, -1, :]
+
+
+            if generation_mode == GenerationMode.GREEDY_SEARCH:  # greedy
+                token = int(torch.argmax(last_token_logits))
+            else:
+                probs = torch.softmax(last_token_logits, dim=-1)
+                token = int(torch.multinomial(probs, num_samples=1))
+
+            output_ids.append(token)
+            if token in stop_token_ids:
+                break
+
+            decoded_output = self.tokenizer.decode(
+                    output_ids,
+                    skip_special_tokens=True,
+                    spaces_between_special_tokens=False,
+                )
+            response = decoded_output.removeprefix(prompt)
+            yield response
+        yield response
--- evaluation/generate.py
+++ evaluation/generate.py
@@ -7,4 +7,102 @@ import torch
 import transformers
 from LLM import LLM
 
+from utils.prompter import PrompterGPT4Tools
 from helpers.ssl import no_ssl_verification
+from ToolLibrary.thirtytools_utils import load_thirty_tools
+from CodeRunner.runner import CodeRunner
+from ToolLibrary.utils import extract_code
+
+if torch.cuda.is_available():
+    device = "cuda"
+else:
+    device = "cpu"
+
+
+def prepare_tools() -> list[dict]:
+    tool_library: list[dict] = load_thirty_tools()
+    return tool_library
+
+
+def prepare_tools_for_runner(tool_list: list) -> dict:
+    tool_dict = {}
+    for tool in tool_list:
+        tool_dict[tool["func_name"]] = tool["func"]
+    return tool_dict
+
+
+def main(
+    test_data_path: str,
+    base_model: str = '',
+    lora_weights: str = '',
+    output_path: str = '',
+    prompt_template_path: str = '',
+    load_8bit: bool = True,
+    load_4bit: bool = False,
+    load_from_local: bool = True,
+    max_new_tokens_default: int = 512,
+):
+    with open(test_data_path, "r") as fin:
+        test_data = json.load(fin)
+    with open(prompt_template_path, 'r') as f:
+        template = f.read()
+    
+    llm = LLM(
+        base_model=base_model,
+        lora_weights=lora_weights,
+        load_8bit=load_8bit,
+        load_in_4bit=load_4bit,
+        load_from_local=load_from_local,
+    )
+    generate_params = {
+        "return_dict_in_generate": True,
+        "output_scores": True,
+        "max_new_tokens": max_new_tokens_default,
+    }
+
+    with no_ssl_verification():
+        prompter = PrompterGPT4Tools(template)
+        tools = prepare_tools_for_runner(prepare_tools())
+        runner = CodeRunner()
+        runner.add_tools(tools)
+        prompts = []
+        for item in test_data:
+            prompt = prompter.generate_prompt(item)
+
+            prompt += "Thought: Do I need to use a tool? Yes\n"
+        
+            steps = 0
+            while True:
+                stop_sequences = ['Output:', llm.tokenizer.eos_token]
+                res = llm.generate(prompt, stop_sequences=stop_sequences)
+                for response in res:
+                    pass
+                print(response)
+                code = extract_code(response)
+                execution_result = runner.run(code)
+                # adding execution result to prompt
+                execution_result = str(execution_result)
+                encoded_execution_result = llm.tokenizer.encode(execution_result, add_special_tokens=False)
+                max_exec_result_len = generate_params['max_new_tokens'] // 2
+                if len(encoded_execution_result) >= max_exec_result_len:
+                        encoded_execution_result = encoded_execution_result[:max_exec_result_len]
+                execution_result = llm.tokenizer.decode(encoded_execution_result)
+                prompt += response + ' ' + execution_result + '\n'
+                if not 'EXEC ERROR' in execution_result or 'Thought: Do I need to use a tool? No' in execution_result:
+                    break
+                else:
+                    steps += 1
+                if steps == 3:
+                    break
+            stop_sequences = ['Output:', llm.tokenizer.eos_token]
+            if 'Thought: Do I need to use a tool? Yes' in prompt:
+                res = llm.generate(prompt, stop_sequences=stop_sequences)
+                for response in res:
+                    pass
+                prompt += response
+            prompts.append({'output': prompt})
+            with open(output_path, 'w') as f:
+                json.dump(prompts, f)
+
+if __name__ == "__main__":
+    fire.Fire(main)
